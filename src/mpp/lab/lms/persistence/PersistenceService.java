67ad4c6cd3a147452f96c54962bde5c3db0552b0
Khangarid <khangarid.davaadorj@gmail.com>
2018-06-07 15:20:32 -0500

    Persistence class update

--- src/mpp/lab/lms/persistence/PersistenceService.java
+++ src/mpp/lab/lms/persistence/PersistenceService.java
@@ -1,5 +1,14 @@
 package mpp.lab.lms.persistence;
 
+import java.util.List;
+
+import mpp.lab.lms.model.Book;
+import mpp.lab.lms.model.Member;
+
 public interface PersistenceService {
 	void persistObject(Object o);
+	List<Book> getBooks();
+	void setBooks(List<Book> books);
+	List<Member> getMembers();
+	void setMembers(List<Member> members);
 }
--- src/mpp/lab/lms/persistence/PersistenceServiceImpl.java
+++ src/mpp/lab/lms/persistence/PersistenceServiceImpl.java
@@ -1,10 +1,32 @@
 package mpp.lab.lms.persistence;
 
+import java.util.List;
+
+import mpp.lab.lms.model.Book;
+import mpp.lab.lms.model.Member;
+
 public class PersistenceServiceImpl implements PersistenceService {
+	private List<Book> books;
+	private List<Member> members;
 
 	@Override
 	public void persistObject(Object o) {
-		System.out.println(o.toString());		
+		System.out.println("Saving - " + o.toString());
 	}
 
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+	public List<Member> getMembers() {
+		return members;
+	}
+
+	public void setMembers(List<Member> members) {
+		this.members = members;
+	}
 }
