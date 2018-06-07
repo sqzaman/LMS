c233c8f90c73ff4f3c863f5368b1164d7ac24f6b
Silas Kaggwa <silakag@gmail.com>
2018-06-07 14:54:22 -0500

    setters and getters CheckoutEntry

--- src/mpp/lab/lms/model/CheckoutEntry.java
+++ src/mpp/lab/lms/model/CheckoutEntry.java
@@ -1,5 +1,28 @@
 package mpp.lab.lms.model;
 
-public class CheckoutEntry {
+import java.util.Date;
 
+public class CheckoutEntry {
+	private BookCopy book;
+	private Date checkoutDate;
+	private Date dueDate;
+	
+	public CheckoutEntry(BookCopy book, Date checkoutDate, Date dueDate) {
+		this.book = book;
+		this.checkoutDate = checkoutDate;
+		this.dueDate = dueDate;
+	}
+
+	public BookCopy getBook() {
+		return book;
+	}
+
+	public Date getCheckoutDate() {
+		return checkoutDate;
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+	
 }
