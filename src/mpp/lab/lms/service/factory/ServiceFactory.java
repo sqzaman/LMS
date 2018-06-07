5711e038b7d994efaee819d362dc4727585628b0
= <=>
2018-06-07 16:36:56 -0500

    Login Service - Registering in the factory method

--- src/mpp/lab/lms/service/factory/ServiceFactory.java
+++ src/mpp/lab/lms/service/factory/ServiceFactory.java
@@ -3,11 +3,14 @@ package mpp.lab.lms.service.factory;
 import mpp.lab.lms.persistence.PersistenceService;
 import mpp.lab.lms.persistence.PersistenceServiceImpl;
 import mpp.lab.lms.service.BookService;
+import mpp.lab.lms.service.LoginService;
 import mpp.lab.lms.service.impl.BookServiceImpl;
+import mpp.lab.lms.service.impl.LoginServiceImpl;
 
 public class ServiceFactory {
 	private volatile static PersistenceService persistenceService;
 	private volatile static BookService bookService;
+	private volatile static LoginService loginService;
 
 	
 	public static PersistenceService getPersistenceService() {
@@ -31,6 +34,17 @@ public class ServiceFactory {
 		}
 		return bookService;
 	}	
+	
+	public static LoginService getLoginService() {
+		if(null == loginService) {
+			synchronized(ServiceFactory.class) {
+				if(null == loginService) {
+					loginService = new LoginServiceImpl();
+				}
+			}
+		}
+		return loginService;
+	}
 	
 }
 
